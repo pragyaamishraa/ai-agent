de85f56d06cdcac6a78b43190e1c81060b77c863
Pragya Mishra <126263327+pragyaamishraa@users.noreply.github.com>
2026-03-31 01:21:55 +0530

    Initial upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "AI Agent is running"
+
+#@app.route('/ask', methods=['POST'])
+@app.route('/ask')
+def ask():
+    question = request.args.get("question")
+
+    response = f"You asked: {question}. This is a simple response."
+
+    return jsonify({"response": response})
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
